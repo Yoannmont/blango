6887b6c17ffbb2164b5f7ea707c3dab91d257e02
Yoann Monteiro <monicploc@gmail.com>
2025-04-20 07:20:10 +0000

    Finish custom filters

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,55 @@
+from django.contrib.auth import get_user_model
+from django import template
+from django.utils.html import escape 
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+
+user_model = get_user_model()
+register = template.Library()
+
+
+def full_name(user):
+    if not isinstance(user, user_model):
+        return ''
+    
+    if user.first_name and user.last_name:
+        return f'{user.first_name} {user.last_name}'
+    
+    return user.username
+
+
+def email_html(user):
+    if not isinstance(user, user_model):
+        return ''
+
+    email_html = f'<a href="mailto:{escape(user.email)}">{escape(user.username)}</a>'
+
+    return mark_safe(email_html)
+
+
+@register.filter
+def author_details(author, current_user):
+    if not isinstance(author, user_model):
+        # return empty string as safe default
+        return ""
+    
+    print(author, current_user)
+
+    if author == current_user:
+        return format_html("<strong>Me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+    
+
+
+    return format_html('{}{}{}', prefix, name, suffix)
