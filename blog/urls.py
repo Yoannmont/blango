a49568d65e661e5fb05db14a18182074ac59691f
Yoann Monteiro <monicploc@gmail.com>
2025-04-21 20:05:53 +0000

    Finish First-Party Django REST API

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ app_name = "blog"
 
 urlpatterns = [path('', index, name="index"),
                path('post/<slug>/', post_detail, name="post-detail"),
-               path('get_ip', get_ip, name="get-ip")
+               path('get_ip/', get_ip, name="get-ip"),
+               path('api/v1/', include('blog.api_urls')),
               ]
