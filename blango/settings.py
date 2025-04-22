b12a0efb07b5cb36467e8156c727a482c31643e9
Yoann Monteiro <monicploc@gmail.com>
2025-04-22 20:01:10 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -131,6 +131,7 @@ class Dev(Configuration):
         'allauth.socialaccount.providers.google',
         'rest_framework',
         'rest_framework.authtoken',
+        'drf_yasg'
     ]
 
     MIDDLEWARE = [
@@ -170,6 +171,13 @@ class Dev(Configuration):
     ]
 
     WSGI_APPLICATION = 'blango.wsgi.application'
+
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
+    }
 
 
     # Database
