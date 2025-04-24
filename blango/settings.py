785377859f000ab3ff97db8a3d5e24d4401105a8
Yoann Monteiro <monicploc@gmail.com>
2025-04-24 12:24:26 +0000

    Finish throttling

--- blango/settings.py
+++ blango/settings.py
@@ -96,6 +96,7 @@ class Dev(Configuration):
     # Authentication
     AUTH_USER_MODEL = "blango_auth.User"
     ACCOUNT_ACTIVATION_DAYS = 7
+    
 
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
@@ -109,7 +110,19 @@ class Dev(Configuration):
         ],
         "DEFAULT_PERMISSION_CLASSES": [
             "rest_framework.permissions.IsAuthenticatedOrReadOnly",
-    ]
+        ]
+        "DEFAULT_THROTTLE_CLASSES": [
+            "blog.api.throttling.AnonSustainedThrottle",
+            "blog.api.throttling.AnonBurstThrottle",
+            "blog.api.throttling.UserSustainedThrottle",
+            "blog.api.throttling.UserBurstThrottle",
+        ],
+        "DEFAULT_THROTTLE_RATES": {
+            "anon_sustained": "500/day",
+            "anon_burst": "10/minute",
+            "user_sustained": "5000/day",
+            "user_burst": "100/minute",
+        },
     }
 
     INSTALLED_APPS = [
